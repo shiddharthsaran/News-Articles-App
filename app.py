56516924a3f3aa420663bda5a4037a0fb39c1260
Shiddharth Saran <47408933+shiddharthsaran@users.noreply.github.com>
2023-02-08 13:04:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ es_news_index_name="all_news_index"
 
 es = Elasticsearch(
 		cloud_id="f7098e2538d04cf3a82420cb52ed7132:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQ1OGE1NTIzODJlMzU0MzNhYWFkYzQxYzI3Y2Q2MDY0OSQ1ZjFlZTEwZmQzMGU0NGYwOGYxMjYzMmNjMmFjMGU4ZQ==",
-		http_auth=("elastic","ZYMAXeNWkqZDkVb9YB2jSQrM"),
-		scheme="https",
-    	port=443
+		basic_auth=("elastic","ZYMAXeNWkqZDkVb9YB2jSQrM")
 	)
 def get_es_read_results(from_result,result_size=10):
 	
